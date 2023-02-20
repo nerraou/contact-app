cf76471c6415a01c68cf0c64841d986e8a40cfc7
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-02-20 19:13:55 +0100

    init contact app backend

---
+++ index.js
@@ -0,0 +1,15 @@
+const app = require("./src/app.js");
+const mongoose = require("mongoose");
+const port = 3000;
+
+// to avoid Deprecation Warning
+mongoose.set("strictQuery", false);
+
+mongoose
+  .connect("mongodb://localhost:27017/myDb")
+  .then(function () {
+    app.listen(port, function () {
+      console.log("Server started");
+    });
+  })
+  .catch(console.log);
